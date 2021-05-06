50d474e9677334a11b1da374b4c3fbfcb1db1730
Tariq <selawi.be@gmail.com>
2021-05-06 16:12:54 +0200

    final weather app

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 <p>
   <img alt="Version" src="https://img.shields.io/badge/version-0.1.0-blue.svg?cacheSeconds=2592000" />
   <img alt="axios" src="https://img.shields.io/npm/v/axios?color=yellow&label=axios&logo=axios&style=flat-square">
-  <img alt="Semantic React UI" src="https://img.shields.io/npm/v/semantic-ui-react?color=green&label=Semantic%20React%20ui">
   <img alt="moment.js" src="https://img.shields.io/npm/v/moment?color=orange&label=moment.js">
   <img alr="react-app" src="https://img.shields.io/npm/v/react?color=blue&label=react">
 </p>
@@ -12,23 +11,18 @@
 ## Install
 
 ```sh
-- npx create-react-app <Application name>
 - npm install axios
 - npm i react-axios
-- npm install semantic-ui-react semantic-ui-css
 - npm install moment --save
-- npm install styled-components --save
-- npm i @fortawesome/react-fontawesome
-- npm i --save @fortawesome/free-solid-svg-icons
 ```
-## Imports 
-* Import the semantic-ui-css in you index.js
-```sh
-import 'semantic-ui-css/semantic.main.css'
-```
+## Tools & API
+- [Bootstrap](https://getbootstrap.com/)
+- [Fontaweosome](https://fontawesome.com/)
+- [Openweather](https://openweathermap.org/)
+- [Unsplash](https://unsplash.com/)
+
 
 ## Usage
-
 ```sh
 npm run start
 ```
--- src/components/weather.js
+++ src/components/weather.js
@@ -1,32 +1,23 @@
 import React, { useState } from "react";
-// import "semantic-ui-css/semantic.main.css";
 import axios from "axios";
-// import { Button } from "semantic-ui-react";
 import moment from "moment";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faCloud,
-  faBolt,
-  faCloudRain,
-  faCloudShowersHeavy,
-  faSnowflake,
-  faSun,
-  faSmog,
-} from "@fortawesome/free-solid-svg-icons";
-import styled from "styled-components";
+import Moment from "react-moment";
+
 const api = {
   key: `${process.env.REACT_APP_API_KEY}`,
   base: `${process.env.REACT_APP_API_URL}`,
+  clientId: "3PIY2U5XdbpRKrD8Y8I9oyAJbOlAEyG7pj1zEZrVW48",
+  // clientId: `${process.env.REACT_APP_UNSPLASH_API_KEY}`,
+  clientUrl: "https://api.unsplash.com/search/photos?page=1",
 };
 
 function Weather() {
   const [query, setQuery] = useState("");
   const [weather, setWeather] = useState({});
+  const [weatherList, setWeatherList] = useState({});
   const [weatherIcon, setWeatherIcon] = useState("");
+  const [client, setClient] = useState({});
 
-  const WeatherIcon = styled.div`
-    color: burlywood;
-  `;
   const refresh = () => {
     window.location.reload();
   };
@@ -40,7 +31,23 @@ function Weather() {
             setWeather(result.data);
             getIcons(result.data);
             setQuery("");
-            console.log(result.data);
+            // console.log(result.data);
+          });
+        await axios
+          .get(
+            `${api.base}/forecast/daily?q=${query}&cnt=6&units=metric&appid=${api.key}`
+          )
+          .then((result) => {
+            setWeatherList(result.data);
+            setQuery("");
+            // console.log(result.data);
+          });
+        await axios
+          .get(`${api.clientUrl}&query=${query}&client_id=${api.clientId}`)
+          .then((result) => {
+            setClient(result.data);
+            setQuery("");
+            // console.log(result.data);
           });
       } catch (error) {
         console.error(error);
@@ -49,25 +56,67 @@ function Weather() {
   };
 
   const getIcons = (weather) => {
-    // console.log(weather);
     if (weather.weather[0].main === "Thunderstorm") {
-      setWeatherIcon(<FontAwesomeIcon icon={faBolt} />);
+      setWeatherIcon(
+        <img
+          src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+          alt="img"
+        />
+      );
     } else if (weather.weather[0].main === "Drizzle") {
-      setWeatherIcon(<FontAwesomeIcon icon={faSnowflake} />);
+      setWeatherIcon(
+        <img
+          src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+          alt="img"
+        />
+      );
     } else if (weather.weather[0].main === "Rain") {
-      setWeatherIcon(<FontAwesomeIcon icon={faCloudShowersHeavy} />);
+      setWeatherIcon(
+        <img
+          src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+          alt="img"
+        />
+      );
     } else if (weather.weather[0].main === "Snow") {
-      setWeatherIcon(<FontAwesomeIcon icon={faCloudRain} />);
+      setWeatherIcon(
+        <img
+          src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+          alt="img"
+        />
+      );
     } else if (weather.weather[0].main === "Clear") {
-      setWeatherIcon(<FontAwesomeIcon icon={faSun} />);
-    } else if (weather.weather[0].main === "Clouds") {
-      setWeatherIcon(<FontAwesomeIcon icon={faCloud} />);
+      setWeatherIcon(
+        <img
+          src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+          alt="img"
+        />
+      );
+    } else if (
+      (
+        <img
+          src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+          alt="img"
+        />
+      )
+    ) {
+      setWeatherIcon(
+        <img
+          src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+          alt="img"
+        />
+      );
     } else {
-      setWeatherIcon(<FontAwesomeIcon icon={faSmog} />);
+      setWeatherIcon(
+        <img
+          src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+          alt="img"
+        />
+      );
     }
   };
+
   return (
-    <main className="App">
+    <main className="app">
       <section>
         <div className="search-box">
           <input
@@ -79,38 +128,134 @@ function Weather() {
             onKeyPress={getWeather}
           />
         </div>
-        <div>
+        <div className="btn-center">
           <button
-            class="btn btn-warning btn-lg-circle btn-lg"
+            className="btn btn-warning btn-lg-circle btn-lg"
             onClick={refresh}
           >
-            <i class="fas fa-umbrella"></i>
+            <i className="fas fa-sync"></i>
           </button>
         </div>
       </section>
-      {typeof weather.main !== "undefined" ? (
-        <div className="location-box">
-          <p className="location">
-            {" "}
-            {weather.name}, {weather.sys.country}
-          </p>
-          <p>
-            {moment().format("dddd")}, <span>{moment().format("LL")}</span>
-          </p>
-          <p className="weather">
-            Temprature: {Math.round(weather.main.temp)} &deg;C
-            <p className="temp">Humidity: {weather.main.humidity} %</p>
-          </p>
-          <div>
-            <WeatherIcon style={{ fontSize: 30, marginTop: 15 }}>
-              {weatherIcon}
-            </WeatherIcon>
-            <p className="description">{weather.weather[0].main}</p>
-          </div>
+      <section className="container-fluid my-5">
+        <div class="row">
+          {typeof weather.main !== "undefined" ? (
+            <div className=" col-lg-5 col-md-5 col-sm-5">
+              <div className="location">
+                {" "}
+                <i className="fas fa-home mx-3"></i>
+                {weather.name}, {weather.sys.country}
+                <p>
+                  <i class="fas fa-calendar-day mx-3"></i>
+                  {moment().format("ddd")}, <span>{moment().format("LL")}</span>
+                </p>
+                <i class="fas fa-temperature-high mx-3 my-2"></i>{" "}
+                {Math.round(weather.main.temp)} &deg;C
+                <p>
+                  <i class="fas fa-percentage mx-3"></i> {weather.main.humidity}{" "}
+                  %
+                </p>
+                <p className="icon-weather mx-4">{weatherIcon}</p>
+                <p className="description mx-4 text-uppercase">
+                  {weather.weather[0].main}
+                </p>
+              </div>
+            </div>
+          ) : (
+            ""
+          )}
+          <section className="col-7">
+            {typeof client.results !== "undefined" ? (
+              <img
+                className="img-background"
+                src={client.results[0].urls.small}
+                alt="img"
+              />
+            ) : (
+              ""
+            )}
+          </section>
         </div>
-      ) : (
-        ""
-      )}
+      </section>
+
+      <section>
+        {typeof weatherList.list !== "undefined" ? (
+          <table className="table text-center">
+            <thead className="thead-dark location">
+              <tr>
+                <th scope="col">
+                  <Moment unix format="ddd">
+                    {weatherList.list[1].dt}
+                  </Moment>
+                </th>
+                <th scope="col">
+                  <Moment unix format="ddd">
+                    {weatherList.list[2].dt}
+                  </Moment>
+                </th>
+                <th scope="col">
+                  <Moment unix format="ddd">
+                    {weatherList.list[3].dt}
+                  </Moment>
+                </th>
+                <th scope="col">
+                  <Moment unix format="ddd">
+                    {weatherList.list[4].dt}
+                  </Moment>
+                </th>
+                <th scope="col">
+                  <Moment unix format="ddd">
+                    {weatherList.list[5].dt}
+                  </Moment>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">
+                  <img
+                    src={`http://openweathermap.org/img/wn/${weatherList.list[1].weather[0].icon}.png`}
+                    alt="img"
+                  />
+                </th>
+                <td>
+                  <img
+                    src={`http://openweathermap.org/img/wn/${weatherList.list[2].weather[0].icon}.png`}
+                    alt="img"
+                  />
+                </td>
+                <td>
+                  <img
+                    src={`http://openweathermap.org/img/wn/${weatherList.list[3].weather[0].icon}.png`}
+                    alt="img"
+                  />
+                </td>
+                <td>
+                  <img
+                    src={`http://openweathermap.org/img/wn/${weatherList.list[4].weather[0].icon}.png`}
+                    alt="img"
+                  />
+                </td>
+                <td>
+                  <img
+                    src={`http://openweathermap.org/img/wn/${weatherList.list[5].weather[0].icon}.png`}
+                    alt="img"
+                  />
+                </td>
+              </tr>
+              <tr className="location">
+                <th>{Math.round(weatherList.list[1].temp.day)} &deg;C</th>
+                <th>{Math.round(weatherList.list[2].temp.day)} &deg;C</th>
+                <th>{Math.round(weatherList.list[3].temp.day)} &deg;C</th>
+                <th>{Math.round(weatherList.list[4].temp.day)} &deg;C</th>
+                <th>{Math.round(weatherList.list[5].temp.day)} &deg;C</th>
+              </tr>
+            </tbody>
+          </table>
+        ) : (
+          ""
+        )}
+      </section>
     </main>
   );
 }
